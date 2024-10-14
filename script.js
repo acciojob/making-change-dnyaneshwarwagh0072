49199eab4327595d04cd22857f3cea8734b79682
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-10-14 22:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const makeChange = (c) => {
-  // your name here
-};
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
+const makeChange = (c) => {
+    let change = {
+        q: 0,
+        d: 0,
+        n: 0,
+        p: 0
+    };
+
+    c = parseInt(c, 10);
+
+    change.q = Math.floor(c / 25);
+    c %= 25;
+
+    change.d = Math.floor(c / 10);
+    c %= 10;
+
+    change.n = Math.floor(c / 5);
+    c %= 5;
+
+    change.p = c;
+
+    return change;
+};
+const c = prompt("Enter c (amount in cents): ");
 alert(JSON.stringify(makeChange(c)));
